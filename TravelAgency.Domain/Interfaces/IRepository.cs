9dec11199dfc8205e912ad6b266d0af7591f0d5c
Тигран Кандилян <tigranprogramm@gmail.com>
2025-09-19 01:40:07 +0300

    Add repository

---
+++ TravelAgency.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace TravelAgency.Domain.Interfaces
+{
+    /// <summary>
+    /// Интерфейс репозитория.
+    /// </summary>
+    public interface IRepository
+    {
+        /// <summary>
+        /// Поиск в репозитории или загрузка по id.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<T?> GetAsync<T>(Guid id, CancellationToken? cancellationToken = null) 
+            where T : class;
+    }
+}
---
+++ TravelAgency.Persistence/Repositories/EfRepository.cs
@@ -0,0 +1,20 @@
+﻿using TravelAgency.Domain.Interfaces;
+
+namespace TravelAgency.Persistence.Repositories
+{
+    public class EfRepository : IRepository
+    {
+        private readonly TravelAgencyContext _context;
+        public EfRepository(TravelAgencyContext context)
+        {
+            _context = context;
+        }
+        public async Task<T?> GetAsync<T>(Guid id, CancellationToken? cancellationToken = null) where T : class
+        {
+            var token = cancellationToken ?? CancellationToken.None;
+
+            return await _context.Set<T>()
+                .FindAsync(new object[] { id }, token);
+        }
+    }
+}
