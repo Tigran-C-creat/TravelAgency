f08df467ccdd98d510161d347e72f91b3e2a62d0
Тигран Кандилян <tigranprogramm@gmail.com>
2025-09-21 13:50:25 +0300

    Add TravelAgency.Shared and Exeptions

---
+++ TravelAgency.Shared/Exeptions/LogicException.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+
+namespace TravelAgency.Shared.Exeptions
+{
+    public class LogicException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.BadRequest;
+        public string? Code { get; set; }
+
+        public LogicException()
+        {
+        }
+
+        public LogicException(string message) : base(message)
+        {
+        }
+
+        public LogicException(string message, string code) : base(message)
+        {
+            Code = code;
+        }
+
+        public LogicException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public LogicException(string message, string code, Exception innerException) : base(message, innerException)
+        {
+            Code = code;
+        }
+
+    }
+}
---
+++ TravelAgency.Shared/Exeptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace TravelAgency.Shared.Exeptions
+{
+    public class NotFoundException : LogicException
+    {
+        public NotFoundException()
+        {
+            StatusCode = HttpStatusCode.NotFound;
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
